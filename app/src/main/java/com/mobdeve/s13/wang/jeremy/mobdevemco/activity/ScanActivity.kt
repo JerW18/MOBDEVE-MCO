7645be2e0af417d7908f2f47ad6d98f3419189f9
alyannacabrera <alyanna_hazel_cabrera@dlsu.edu.ph>
2024-11-19 19:02:48 +0800

    Added back button in scan

--- app/src/main/java/com/mobdeve/s13/wang/jeremy/mobdevemco/activity/ScanActivity.kt
+++ app/src/main/java/com/mobdeve/s13/wang/jeremy/mobdevemco/activity/ScanActivity.kt
@@ -56,6 +56,9 @@ class ScanActivity : ComponentActivity() {
         binding.btnScanPullOut.setOnClickListener {
             finish()
         }
+        binding.ivScanBack.setOnClickListener {
+            finish()
+        }
 
         binding.ivScanMinus.setOnClickListener {
             if (binding.etScanQty.text.toString().toInt() > 1) {
